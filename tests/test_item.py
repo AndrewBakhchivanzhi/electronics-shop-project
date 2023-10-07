b6a7311444d655444aa59edd522ce7e56b21254d
Andrew Bakhchivanzhi <bakhcha_ml_moscow@mail.ru>
2023-10-07 11:56:30 +0300

    add_tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+
+def test_apply_discount():
+    Item.pay_rate = 0.75
+    item1.apply_discount()
+    assert item1.price == 7500
+
