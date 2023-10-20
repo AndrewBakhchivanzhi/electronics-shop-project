87242e013039089dc110dfa88447d2a2beef7426
Andrew <bakhcha_ml@mail.ru>
2023-10-20 08:59:02 +0300

    homework_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -10,14 +11,29 @@ class Item:
         Создание экземпляра класса item
 
         """
-        self.name = name  # Название товара
+        self._name = name  # Название товара
         self.price = price  # Цена за единицу товара
         self.quantity = quantity  # Количество товара в магазине
+        Item.all.append(self)  # Хранение экземпляров класса
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        """
+        Проверка на количество символов больше 10
+        """
+        if len(value) <= 10:
+            self._name = value
+        else:
+            self._name = value[:10]
+
 
     def calculate_total_price(self) -> float:
         """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
+        Рассчитывает общую стоимость конкретного товара в магазине
         """
         return self.price * self.quantity
 
@@ -26,5 +42,23 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file):
+        items = []
+        with open(file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+            cls.all = items
+
+
+    @staticmethod
+    def string_to_number(number: str):
+        return int(float(number))
 
 
