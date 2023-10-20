cd3505b0114afbadbd03b7193b6cc5218ab49308
Andrew <bakhcha_ml@mail.ru>
2023-10-20 09:14:15 +0300

    homework_2_tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import csv
 
 item1 = Item("Смартфон", 10000, 20)
 def test_calculate_total_price():
@@ -9,4 +10,14 @@ def test_apply_discount():
     Item.pay_rate = 0.75
     item1.apply_discount()
     assert item1.price == 7500
+
+def test_string_to_number():
+    assert Item.string_to_number('6') == 6
+    assert Item.string_to_number('7.1') == 7
+    assert Item.string_to_number('8.9') == 8
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('C:\\Users\\Andrew\\PycharmProjects\\electronics-shop-project\\src\\items.csv')
+    assert len(Item.all) == 5
 
